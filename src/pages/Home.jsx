67a5936e5d3950a9e7a7a25f7d5d4eebf099cd46
Madhur Narang <71398066+MadhurNarang@users.noreply.github.com>
2023-09-22 08:50:48 +0530

    Updated name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -169,7 +169,7 @@ function Home() {
             
             <div className="flex flex-col items-start gap-10 lg:w-[40%]">
               <div className="text-[16px]">
-                The modern StudyNotion is the dictates its own terms. Today, to
+                The modern IndiaNotion is the dictates its own terms. Today, to
                 be a competitive specialist requires more than professional
                 skills.
               </div>
